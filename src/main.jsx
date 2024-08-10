ff77744f12f596bb6ae67eababadafcd8e3576d1
Ranaweera-HD <himathdanindu2006@gmail.com>
2024-08-10 09:11:49 +0530

    Add Routing

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,39 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+  import Header from './Pages/jsx/Header.jsx';
+  import Home from './Pages/jsx/Home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/header",
+    element: <Header />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  }
+  // ,
+  // {
+  //   path: "/header",
+  //   element: <Header />,
+  // },
+  // {
+  //   path: "/header",
+  //   element: <Header />,
+  // }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
